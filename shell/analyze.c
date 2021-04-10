d5c80db2b126c3f11493125c8817f360cf14cf80
Georgiy Lebedev <curiousgeorgiy@gmail.com>
2021-04-11 00:31:32 +0300

    [analyze.c]

--- shell/analyze.c
+++ shell/analyze.c
@@ -1,86 +1,105 @@
-#include "analyzer.h"
+#include "analyze.h"
 
 #include <assert.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
-#include "command.h"
-#include "errors.h"
+#include "pipeline.h"
+#include "shell_errors.h"
 
-static bool analyze_exec(struct executable *exec, const struct token **tokens, const struct token *tokens_end);
-static size_t cnt_program_args(const struct token *tokens, const struct token *tokens_end);
-static bool analyze_redir_op(struct redirection *redir, const struct token **tokens, const struct token *tokens_end);
-static bool analyze_pipe(const struct token **tokens, const struct token *tokens_end);
+static enum shell_status analyze_cmd(struct command *cmd, const struct token **toks, const struct token *toks_end);
+static size_t cnt_cmd_args(const struct token *toks, const struct token *toks_end);
+static enum shell_status analyze_redir_op(struct redirection *redir, const struct token **toks, const struct token *toks_end);
 
-bool analyze(struct command *const cmd, const struct token *tokens, const size_t n_tokens, const size_t n_execs)
+enum shell_status analyze(struct list *const lst, const struct token *toks, const size_t n_toks)
 {
-    assert(cmd != NULL);
+    assert(lst != NULL);
+    assert(toks != NULL);
 
-    cmd->execs = calloc(n_execs, sizeof(*cmd->execs));
-    if (cmd->execs == NULL) HANDLE_SYS_ERROR({ return NULL; }, "calloc");
-
+    const struct token *const toks_end = toks + n_toks;
     size_t cmd_idx = 0;
-    const struct token *const tokens_end = tokens + n_tokens;
-    while (tokens != tokens_end) {
-        assert(tokens != NULL);
+    size_t pipeline_idx = 0;
+    enum shell_status stat = SHELL_SUCCESS;
+    while (toks != toks_end) {
+        assert(toks != NULL);
 
-        switch (tokens->type) {
-            case EXEC_NAME:
-                if (!analyze_exec(&cmd->execs[cmd_idx++], &tokens, tokens_end)) return false;
+        switch (toks->type) {
+            case CMD_NAME:
+                stat = analyze_cmd(&lst->pipelines[pipeline_idx].cmds[cmd_idx], &toks, toks_end);
+                if (stat != SHELL_SUCCESS) return stat;
 
                 break;
-            case EXEC_ARG:
-                HANDLE_LOGIC_ERROR({ return false; }, "expected executable name, got executable argument \"%s\"\n", tokens->str);
+            case CMD_ARG:
+                HANDLE_LOGIC_ERROR({ return false; }, "expected command name, got command argument \"%s\"\n", toks->str);
 
                 break;
             case REDIR_OP:
-                if (!analyze_redir_op(&cmd->redir, &tokens, tokens_end)) return false;
+                stat = analyze_redir_op(&lst->pipelines[pipeline_idx].redir, &toks, toks_end);
+                if (stat != SHELL_SUCCESS) return stat;
 
                 break;
             case REDIR_FILE_NAME:
-                HANDLE_LOGIC_ERROR({ return false; }, "expected redirection operator, got redirection file name \"%s\"\n", tokens->str);
+                HANDLE_LOGIC_ERROR({ return SHELL_LOGIC_ERROR; },
+                                   "expected redirection operator, got redirection file name \"%s\"\n", toks->str);
 
                 break;
             case PIPE_OP:
-                if (!analyze_pipe(&tokens, tokens_end)) return false;
+                ++cmd_idx;
+                ++toks;
 
                 break;
+            case LIST_OP:
+                lst->pipelines[pipeline_idx].list_op = toks->op[0];
+                cmd_idx = 0;
+                ++pipeline_idx;
+                ++toks;
+
+                break;
+            case BG_OP:
+                if (++toks != toks_end) HANDLE_LOGIC_ERROR({ return SHELL_LOGIC_ERROR; },
+                                                           "expected end of line after background operator\n");
+                lst->bg = true;
+
+                break;
+            default:
+                HANDLE_LOGIC_ERROR({ return SHELL_LOGIC_ERROR; },
+                                   "unknown token type '%d'", toks->type);
         }
     }
 
-    return true;
+    return SHELL_SUCCESS;
 }
 
-bool analyze_exec(struct executable *const exec, const struct token **tokens, const struct token *tokens_end)
+enum shell_status analyze_cmd(struct command *cmd, const struct token **toks, const struct token *toks_end)
 {
-    assert(exec != NULL);
-    assert(tokens != NULL);
-    assert(tokens_end != NULL);
+    assert(cmd != NULL);
+    assert(toks != NULL);
+    assert(toks_end != NULL);
 
-    exec->argv = calloc(cnt_program_args(*tokens, tokens_end) + 2, sizeof(*exec->argv));
-    if (exec->argv == NULL) HANDLE_SYS_ERROR({ return false; }, "calloc");
+    cmd->argv = calloc(cnt_cmd_args(*toks + 1, toks_end) + 2, sizeof(*cmd->argv));
+    if (cmd->argv == NULL) HANDLE_SYS_ERROR({ return SHELL_SYS_ERROR; }, "calloc: %s\n");
 
-    exec->argv[0] = ((*tokens)++)->str;
+    cmd->argv[0] = ((*toks)++)->str;
 
     size_t argv_idx = 1;
-    while ((*tokens != tokens_end) && ((*tokens)->type == EXEC_ARG)) {
-        assert(*tokens != NULL);
+    while ((*toks != toks_end) && ((*toks)->type == CMD_ARG)) {
+        assert(*toks != NULL);
 
-        exec->argv[argv_idx++] = ((*tokens)++)->str;
+        cmd->argv[argv_idx++] = ((*toks)++)->str;
     }
 
-    return true;
+    return SHELL_SUCCESS;
 }
 
-size_t cnt_program_args(const struct token *tokens, const struct token *const tokens_end)
+size_t cnt_cmd_args(const struct token *toks, const struct token *toks_end)
 {
-    assert(tokens != NULL);
-    assert(tokens_end != NULL);
+    assert(toks != NULL);
+    assert(toks_end != NULL);
 
     size_t argc = 0;
-    while ((tokens != tokens_end) && (tokens->type == EXEC_ARG)) {
-        assert(tokens != NULL);
+    while ((toks != toks_end) && ((toks++)->type == CMD_ARG)) {
+        assert(toks != NULL);
 
         ++argc;
     }
@@ -88,39 +107,23 @@ size_t cnt_program_args(const struct token *tokens, const struct token *const to
     return argc;
 }
 
-bool analyze_redir_op(struct redirection *const redir, const struct token **tokens, const struct token *const tokens_end)
+enum shell_status analyze_redir_op(struct redirection *const redir, const struct token **toks, const struct token *const toks_end)
 {
     assert(redir != NULL);
-    assert(tokens != NULL);
-    assert(*tokens != NULL);
-    assert(tokens_end != NULL);
-    assert(*tokens != tokens_end);
+    assert(toks != NULL);
+    assert(*toks != NULL);
+    assert(toks_end != NULL);
+    assert(*toks != toks_end);
 
-    if (*tokens + 2 != tokens_end) {
-        HANDLE_LOGIC_ERROR({ return false; }, "syntax error near unexpected token '%s'\n", (*tokens)->redir_op);
-    }
+    if (*toks + 2 != toks_end) HANDLE_LOGIC_ERROR({ return SHELL_LOGIC_ERROR; }, "syntax error near unexpected token '%s'\n", (*toks)->op);
 
-    strncpy(redir->op, ((*tokens)++)->redir_op, sizeof(redir->op));
+    strncpy(redir->op, ((*toks)++)->op, sizeof(redir->op));
 
-    if ((*tokens == tokens_end) || (*tokens)->type != REDIR_FILE_NAME) {
-        HANDLE_LOGIC_ERROR({ return false; }, "syntax error near redirection operator '%s'\n", redir->op);
-    }
+    if ((*toks == toks_end) || (*toks)->type != REDIR_FILE_NAME) HANDLE_LOGIC_ERROR({ return SHELL_LOGIC_ERROR; },
+                                                                                    "syntax error near redirection operator '%s'\n",
+                                                                                    redir->op);
 
-    redir->file_name = ((*tokens)++)->str;
+    redir->file_name = ((*toks)++)->str;
 
-    return true;
-}
-
-bool analyze_pipe(const struct token **tokens, const struct token *const tokens_end)
-{
-    assert(tokens != NULL);
-    assert(*tokens != NULL);
-    assert(tokens_end != NULL);
-    assert(*tokens != tokens_end);
-
-    if ((((*tokens)[-1].type != EXEC_ARG) && ((*tokens)[-1].type != EXEC_NAME)) || ((*tokens)++ == tokens_end) || ((*tokens)->type != EXEC_NAME)) {
-        HANDLE_LOGIC_ERROR({ return false; }, "expected pipe operator argument\n");
-    }
-
-    return true;
+    return SHELL_SUCCESS;
 }
