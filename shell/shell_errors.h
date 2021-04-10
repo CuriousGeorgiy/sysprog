2d7a4ead6bb20622da754971c54973421908b41f
Georgiy Lebedev <curiousgeorgiy@gmail.com>
2021-04-11 00:28:34 +0300

    [shell_errors.h]

--- shell/shell_errors.h
+++ shell/shell_errors.h
@@ -1,13 +1,15 @@
 #ifndef SHELL_ERRORS_H
 #define SHELL_ERRORS_H
 
-#include <stdbool.h>
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 
-enum status {
-    OK,
-    ESYS,
-    ELOGIC
+enum shell_status {
+    SHELL_SUCCESS,
+    SHELL_SYS_ERROR,
+    SHELL_LOGIC_ERROR,
+    SHELL_CHILD_PROCESS_ERROR
 };
 
 /*!
@@ -16,13 +18,13 @@ enum status {
  * @details prints the error description calling by calling perror and then executes code for
  * handling error
  */
-#define HANDLE_SYS_ERROR(error_handling_src_code, error_description) \
-{                                                                    \
-    perror(error_description);                                       \
-                                                                     \
-    {                                                                \
-        error_handling_src_code                                      \
-    }                                                                \
+#define HANDLE_SYS_ERROR(error_handling_src_code, ...) \
+{                                                      \
+    fprintf(stderr, __VA_ARGS__, strerror(errno));     \
+                                                       \
+    {                                                  \
+        error_handling_src_code                        \
+    }                                                  \
 } do {} while (false)
 
 #define HANDLE_LOGIC_ERROR(error_handling_src_code, ...) \
